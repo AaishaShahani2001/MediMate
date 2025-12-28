8fb63f42bd264403ffd8b72e2ac4d8b40f2ded98
AaishaShahani2001 <shahani6360@gmail.com>
2025-12-28 20:39:53 +0530

    feat: modified become a doctor details with nic,license seoeration

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import path from "path";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -15,6 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 // routes
 app.use("/api/auth", authRoutes);
